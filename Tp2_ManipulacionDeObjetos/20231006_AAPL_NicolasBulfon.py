3ee841acf724b174d7dcdc95373735f3ef2c9289
nbulfon <nicolas.bulfon@gmail.com>
2023-10-04 18:57:04 -0300

    updates

---
+++ Tp2_ManipulacionDeObjetos/20231006_AAPL_NicolasBulfon.py
@@ -0,0 +1,17 @@
+# Economía Computacional
+# Trabajo práctico: Manipulación de objetos
+#---------------------------------------------
+
+#Con el archivo "AAPL.csv" que contiene información de la cotización diaria durante 5 años de las acciones de la empresa APPLE, genere un script en Spyder para realizar las siguientes operaciones de manipulación de datos.
+#Una vez que redacte el Script, envíelo adjunto con el siguiente nombre: "20231006_AAPL_[NombreyApellido].py".
+
+
+#1) Carga del archivo "AAPL.csv".
+#2) Indexar el archivo con la columna de fechas.
+#3) Generar un subconjunto de datos (ej. subset1) en que se muestren solamente las filas correspondientes al año 2020.
+#4) Genere sobre el subconjunto "subset1", una estadística descriptiva de cada columna.
+#5) Obtenga un subconjunto (ej. subset2 ó dataset2) de todo el período, pero en que sólo se muestren las columnas de precios de apertura, cierre y volumen operado. Utilice para armar el subconjunto los índices de columna Python (numéricos).
+#6) En un subconjunto (ej. subset3) obtenga el valor de cierre para el día 9/11/2018.
+#7) Obtenga un subconjunto (ej. subset 4) para los años 2018 y 2019, con las columnas precio máximo, mínimo y precio de cierre ajustado.
+#8) Genere un gráfico de líneas para todo el período considerado con el precio de cierre de la acción, y el precio de apertura. El gráfico deberá contener título, la leyenda de los ejes, como así también la leyenda del significado de cada gráfico superpuesto.
+#9) Hacer un informe en Word con todos los datos obtenidos y gráficos, pasarlo a pdf y enviarlo por email junto con el Script. En el informe detallar el nombre del alumno y fecha.
